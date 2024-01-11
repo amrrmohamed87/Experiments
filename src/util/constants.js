51bf58c369786440228d49e95f4b43d52cf44af6
Amr <frank1276smith@gmail.com>
2024-01-11 22:47:49 +0200

    adding constantsJs for data purposes.

---
+++ src/util/constants.js
@@ -0,0 +1,11 @@
+// Contact Us
+export const  frontendEngineers = [
+    {name: "Amr Muhammad", specialization: "Frontend Software Engineer", email:"amrrmohamed87@gmail.com"},
+    {name: "Ramez Hossam", specialization: "Frontend Web Developer", email: "ramezhossam92@gmail.com"},
+    {name: "Mohamed Saber" , specialization: "Frontend Mobile Developer", email: "elmohandesstamera@gamil.com"},
+];
+
+export const backendEngineers = [
+    {name: "Abdelrahman Khaled", specialization: "Backend Engineer", email: "keshko123456@gmail.com"},
+    {name: "Ibrahim Elfatarany", specialization: "Backend Engineer", email: "ibrahimelfatarny75@gmail.com"},
+];
